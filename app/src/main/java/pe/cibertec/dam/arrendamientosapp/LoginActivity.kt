29b790ef88392b65cde22e7428e8b6f5dc7001e8
tonsam1810 <132167554+tonsam1810@users.noreply.github.com>
2024-03-02 22:41:37 -0500

    Creacion de Activity para testear barra de navegacion

--- app/src/main/java/pe/cibertec/dam/arrendamientosapp/LoginActivity.kt
+++ app/src/main/java/pe/cibertec/dam/arrendamientosapp/LoginActivity.kt
@@ -16,33 +16,33 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loginManager: LoginManager
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        this.setContentView(R.layout.activity_login)
 
-        usernameEditText=findViewById(R.id.txtUsername)
-        passwordEditText=findViewById(R.id.txtPassword)
-        btnLogin=findViewById(R.id.btnLogin)
+        usernameEditText = findViewById(R.id.txtUsername)
+        passwordEditText = findViewById(R.id.txtPassword)
+        btnLogin = findViewById(R.id.btnLogin)
 
         // Inicializar el administrador de inicio de sesión
         loginManager = LoginManager(this)
 
         // Configurar el click listener del botón de inicio de sesión
-                btnLogin.setOnClickListener {
-                    val username = usernameEditText.text.toString()
-                    val password = passwordEditText.text.toString()
+        btnLogin.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
 
-                    if (loginManager.login(username, password)) {
-                        // Si el inicio de sesión es exitoso, navegar a la siguiente actividad
-                        startActivity(Intent(this, InicioActivity::class.java))
-                        finish()
-                    } else {
-                        // Si el inicio de sesión falla, mostrar un mensaje de error al usuario
-                        Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
-                    }
-                }
+            if (loginManager.login(username, password)) {
+                // Si el inicio de sesión es exitoso, navegar a la siguiente actividad
+                startActivity(Intent(this, InicioActivity::class.java))
+                finish()
+            } else {
+                // Si el inicio de sesión falla, mostrar un mensaje de error al usuario
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
+
 
 
---
+++ app/src/main/java/pe/cibertec/dam/arrendamientosapp/PagosActivity.kt
@@ -0,0 +1,11 @@
+package pe.cibertec.dam.arrendamientosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PagosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagos)
+    }
+}
