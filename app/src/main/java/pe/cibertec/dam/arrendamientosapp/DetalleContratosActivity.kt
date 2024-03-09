8b0698e912d1d7908117bb7f771e197664505611
Carlos Alberto Campos Miñano <149217468+TheMagus08@users.noreply.github.com>
2024-03-09 05:33:10 -0500

    cambios
    dada

---
+++ app/src/main/java/pe/cibertec/dam/arrendamientosapp/DetalleContratosActivity.kt
@@ -0,0 +1,12 @@
+package pe.cibertec.dam.arrendamientosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.cibertec.dam.arrendamientosapp.R
+
+class DetalleContratosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_contratos)
+    }
+}
