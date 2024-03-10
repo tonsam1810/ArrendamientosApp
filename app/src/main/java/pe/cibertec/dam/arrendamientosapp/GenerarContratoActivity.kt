17f5f545d455d1c585f07a27f34bc3deb835672a
Carlos Alberto Campos Miñano <149217468+TheMagus08@users.noreply.github.com>
2024-03-09 22:47:53 -0500

    carlos
    vistas

---
+++ app/src/main/java/pe/cibertec/dam/arrendamientosapp/GenerarContratoActivity.kt
@@ -0,0 +1,11 @@
+package pe.cibertec.dam.arrendamientosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GenerarContratoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generar_contrato)
+    }
+}
